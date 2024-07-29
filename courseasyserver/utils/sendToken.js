a513e766b89a3e1c9233300689d898ad851552e1
Sagar Bhatia <sagarbhatia722@gmail.com>
2024-07-29 15:26:00 +0530

    https false

--- courseasyserver/utils/sendToken.js
+++ courseasyserver/utils/sendToken.js
@@ -3,7 +3,7 @@ export const sendToken = (res, user, message, statusCode = 200) => {
   const options = {
     // maxAge: new Date(Date.now() + 15 * 24 * 60 * 60 * 1000),
     maxAge: 1000 * 60 * 60 * 48,
-    httpOnly: true,
+    httpOnly: false,
     secure: true,
     sameSite: "none",
   };
